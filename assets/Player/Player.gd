98d2306b01966ca6fd3691f7575d24e53c7303dd
navisraditya <navisraditya@gmail.com>
2024-04-22 14:13:34 +0700

    player created

---
+++ assets/Player/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody
+
+export var speed = 10
+export var acceleration = 5
+export var gravity = 0.98
+export var jump_power = 30
+export var mouse_sensitivity = 0.3
+
+onready var head = $Head
+
+var velocity = Vector3()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _physics_process(delta):
+	var head_basis = head.get_global_transform().basis
+
+	var movement_vector = Vector3()
+	if Input.is_action_pressed("movement_forward"):
+		movement_vector -= head_basis.z
+	if Input.is_action_pressed("movement_backward"):
+		movement_vector += head_basis.z
+	if Input.is_action_pressed("movement_left"):
+		movement_vector -= head_basis.x
+	if Input.is_action_pressed("movement_right"):
+		movement_vector += head_basis.x
+
+	movement_vector = movement_vector.normalized()
+
+	velocity = velocity.linear_interpolate(movement_vector * speed, acceleration * delta)
+	velocity.y -= gravity
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += jump_power
+
+	velocity = move_and_slide(velocity, Vector3.UP)
