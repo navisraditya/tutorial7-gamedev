77b83bf21abd5d98d52f293ccec2f7d3ffec1b9a
navisraditya <navisraditya@gmail.com>
2024-04-23 22:25:24 +0700

    tutorial done, readme not

--- assets/Player/Player.gd
+++ assets/Player/Player.gd
@@ -7,14 +7,23 @@ export var jump_power = 30
 export var mouse_sensitivity = 0.3
 
 onready var head = $Head
+onready var camera = $Head/Camera
 
 var velocity = Vector3()
+var camera_x_rotation = 0
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
-	pass
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+
+		var x_delta = event.relative.y * mouse_sensitivity
+		if camera_x_rotation + x_delta > -90 and camera_x_rotation + x_delta < 90:
+			camera.rotate_x(deg2rad(-x_delta))
+			camera_x_rotation += x_delta
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -22,7 +31,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	var head_basis = head.get_global_transform().basis
-
+	
 	var movement_vector = Vector3()
 	if Input.is_action_pressed("movement_forward"):
 		movement_vector -= head_basis.z
